0057a964d941914e62120a8588f731456475515c
Alessandro Masera <maseradb@gmail.com>
2023-03-27 18:17:36 -0300

    Adjusts to generate upsert jobs

--- data_generation.py
+++ data_generation.py
@@ -11,11 +11,11 @@ cs = URL_ONP
 
 connection = oracledb.connect(user=un, password=pw, dsn=cs)
 cursor = connection.cursor()
-for i in range (1, 1000000):	
+for i in range (1, 1000000):
 	if (i % 5000==0): print(i)
 	cursor.execute("COMMIT")
-	v1   = ( ''.join(random.choice(letters) for i in range(20)) )
-	v2	 = ( ''.join(random.choice(letters) for i in range(20)) )
+	v1   = (''.join(random.choice(letters) for i in range(20)))
+	v2	 = (''.join(random.choice(letters) for i in range(20)))
 	cursor.execute("INSERT INTO BIGTABLE VALUES (MASERA.BIGTABLE_SEQ.NEXTVAL,:sv1,:sv2,SYSDATE-DBMS_RANDOM.value(0,366))", sv1=v1,sv2=v2)
 connection.commit()
 cursor.close()
--- testes4.py
+++ testes4.py
@@ -21,32 +21,47 @@ updates=0
 connection = oracledb.connect(user=un, password=pw, dsn=cs)
 cursor = connection.cursor()
 
-print('Start Date:')
-print(datetime.now())
+cursor.execute("SELECT COUNT(1) FROM MASERA.BIGTABLE")
+totalRecordsCursor = cursor.fetchone()
+for row in totalRecordsCursor:
+    totalRecords = row
+    print('Total records before execution:',totalRecords)
+
+queryRange = totalRecords + (totalRecords /20)
+
+totalQueries = totalRecords/100
+print('Upserts to be executed: ', int(totalQueries))
+
+maxQueryInterval = 86400 / queryRange
+minQueryInterval = 345600 / queryRange
+print('Minumum time between queries: ',minQueryInterval)
+print('Maximum time between queries: ',maxQueryInterval)
+
+startTime = datetime.now()
+print('Starting processes of upserts:',startTime)
+
 
 # Loop for insert and updates generation
-for i in range(1,10000):
-    cursor.execute("COMMIT")
-    waittiming = int(randint(3,8))
-    #time.sleep(waittiming)
-    v1   = ( ''.join(random.choice(letters) for i in range(20)) )
-    v2	 = ( ''.join(random.choice(letters) for i in range(20)) )    
-    GENID = int(randint(1,1200000))
-    cursor.execute("SELECT COUNT(1) FROM MASERA.BIGTABLE WHERE ID= :TEMPID",TEMPID=GENID)
-    control = cursor.fetchone()
-    for row in control:
-        control = row
-    if control == 1:
-        updates = updates +1
-        cursor.execute("UPDATE MASERA.BIGTABLE SET COL1=:sv1,COL2=:sv2,DATA_REF=CURRENT_DATE WHERE ID=:TEMPID",sv1=v1,sv2=v2,TEMPID=GENID)
-    else:
-        inserts = inserts +1
-        cursor.execute("INSERT INTO MASERA.BIGTABLE VALUES (MASERA.BIGTABLE_SEQ.NEXTVAL,:sv1,:sv2,CURRENT_DATE)", sv1=v1,sv2=v2)
+#for i in range(1,int(totalQueries)):
+#     cursor.execute("COMMIT")
+#     waittiming = int(randint(minQueryInterval,maxQueryInterval))
+#     #time.sleep(waittiming)
+#     v1   = ( ''.join(random.choice(letters) for i in range(20)) )
+#     v2	 = ( ''.join(random.choice(letters) for i in range(20)) )    
+#     GENID = int(randint(1,queryRange))
+#     cursor.execute("SELECT COUNT(1) FROM MASERA.BIGTABLE WHERE ID= :TEMPID",TEMPID=GENID)
+#     control = cursor.fetchone()
+#     for row in control:
+#         control = row
+#     if control == 1:
+#         updates = updates +1
+#         cursor.execute("UPDATE MASERA.BIGTABLE SET COL1=:sv1,COL2=:sv2,DATA_REF=CURRENT_DATE WHERE ID=:TEMPID",sv1=v1,sv2=v2,TEMPID=GENID)
+#     else:
+#         inserts = inserts +1
+#         cursor.execute("INSERT INTO MASERA.BIGTABLE VALUES (MASERA.BIGTABLE_SEQ.NEXTVAL,:sv1,:sv2,CURRENT_DATE)", sv1=v1,sv2=v2)
 
 # Total results
-print('Finish Date:')
-print(datetime.now())
-print('Total Inserts:')
-print(inserts)
-print('Total Updates:')
-print(updates)
+finishTime = datetime.now()
+print('Hour of conclusion:',finishTime)
+# print('Total Inserts: ',inserts)
+# print('Total Updates:',updates)
