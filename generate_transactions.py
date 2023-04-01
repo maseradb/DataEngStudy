50a01067f6be3162d07060e1626978f706f824a0
Alessandro Masera <maseradb@gmail.com>
2023-03-31 22:51:41 -0300

    Adjust script to use variable as username

--- generate_transactions.py
+++ generate_transactions.py
@@ -21,7 +21,7 @@ updates=0
 connection = oracledb.connect(user=un, password=pw, dsn=cs)
 cursor = connection.cursor()
 
-cursor.execute("SELECT COUNT(1) FROM MASERA.BIGTABLE")
+cursor.execute(f"SELECT COUNT(1) FROM {un}.BIGTABLE")
 totalRecordsCursor = cursor.fetchone()
 for row in totalRecordsCursor:
     totalRecords = row
@@ -49,16 +49,16 @@ for i in range(1,int(totalQueries)):
     v1   = ( ''.join(random.choice(letters) for i in range(20)) )
     v2	 = ( ''.join(random.choice(letters) for i in range(20)) )    
     GENID = int(randint(1,queryRange))
-    cursor.execute("SELECT COUNT(1) FROM MASERA.BIGTABLE WHERE ID= :TEMPID",TEMPID=GENID)
+    cursor.execute(f"SELECT COUNT(1) FROM {un}.BIGTABLE WHERE ID= :TEMPID",TEMPID=GENID)
     control = cursor.fetchone()
     for row in control:
         control = row
     if control == 1:
         updates = updates +1
-        cursor.execute("UPDATE MASERA.BIGTABLE SET COL1=:sv1,COL2=:sv2,DATA_REF=CURRENT_DATE WHERE ID=:TEMPID",sv1=v1,sv2=v2,TEMPID=GENID)
+        cursor.execute(f"UPDATE {un}.BIGTABLE SET COL1=:sv1,COL2=:sv2,DATA_REF=CURRENT_DATE WHERE ID=:TEMPID",sv1=v1,sv2=v2,TEMPID=GENID)
     else:
         inserts = inserts +1
-        cursor.execute("INSERT INTO MASERA.BIGTABLE VALUES (MASERA.BIGTABLE_SEQ.NEXTVAL,:sv1,:sv2,CURRENT_DATE)", sv1=v1,sv2=v2)
+        cursor.execute(f"INSERT INTO {un}.BIGTABLE VALUES ({un}.BIGTABLE_SEQ.NEXTVAL,:sv1,:sv2,CURRENT_DATE)", sv1=v1,sv2=v2)
 
 # Total results
 finishTime = datetime.now()
