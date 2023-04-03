d7a0ed089ee9940848cd5343bdcfa70b3fb38411
Alessandro Masera <maseradb@gmail.com>
2023-04-03 15:07:33 -0300

    Silver table variable ajustments

--- sql_to_delta_full_silver.py
+++ sql_to_delta_full_silver.py
@@ -2,7 +2,7 @@
 from delta.tables import *
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
-import credentials
+from credentials import *
 import pyspark.sql.functions as f
 from datetime import datetime
 
@@ -10,21 +10,24 @@ from datetime import datetime
 # init application
 if __name__ == '__main__':
 
+    bucketprefix='maseradb-delta'
+    jarsHome='/home/maseradb/DataEngStudy'
+
     # init session
     # set configs
     spark = SparkSession \
         .builder \
         .appName('PoC - Lakehouse - GCP') \
         .master('local[*]')\
-        .config("spark.jars", "/home/maseradb/Projects/gcs-connector-hadoop2-latest.jar") \
+        .config("spark.jars", f"{jarsHome}/gcs-connector-hadoop2-latest.jar") \
         .config("spark.hadoop.google.cloud.auth.service.account.enable", "true") \
         .config("spark.jars.packages", "io.delta:delta-core_2.12:1.2.1")\
-        .config("spark.hadoop.google.cloud.auth.service.account.json.keyfile", credentials.GCS_KEY)\
-        .config('spark.driver.extraClassPath', "/home/maseradb/Projects/*")\
+        .config("spark.hadoop.google.cloud.auth.service.account.json.keyfile", GCS_KEY)\
+        .config('spark.driver.extraClassPath', f"{jarsHome}/*")\
         .config('spark.delta.logStore.gs.impl','io.delta.storage.GCSLogStore')\
         .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
         .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
-        .config('temporaryGcsBucket', 'gs://maseradb-stage/')\
+        .config('temporaryGcsBucket', f'gs://{bucketprefix}-stage/')\
         .getOrCreate()
     
     # show configured parameters
@@ -34,7 +37,7 @@ if __name__ == '__main__':
     #spark.sparkContext.setLogLevel("INFO")
 
     # read table from bronze
-    deltaTable = DeltaTable.forPath(spark, "gs://maseradb-bronze/bigtable")
+    deltaTable = DeltaTable.forPath(spark, f"gs://{bucketprefix}-bronze/bigtable")
     df = deltaTable.toDF()
     
     # adjust data
@@ -44,10 +47,6 @@ if __name__ == '__main__':
     df = df.select('ID','COL1','COL2','DATA_REF')\
         .where(f.year(f.col('DATA_REF')) == actual_year)\
         .where(f.month(f.col('DATA_REF')) == actual_month)
-    
-
-    # print data
-    df.show()    
 
     # create delta table on silver
     DeltaTable.createIfNotExists(spark) \
@@ -56,11 +55,11 @@ if __name__ == '__main__':
         .addColumn("COL1", "STRING") \
         .addColumn("COL2", "STRING") \
         .addColumn("DATA_REF", "TIMESTAMP") \
-        .location(f"gs://maseradb-silver/bigtable_last_month-{actual_year}-{actual_month}") \
+        .location(f"gs://{bucketprefix}-silver/bigtable_last_month-{actual_year}-{actual_month}") \
         .execute()
 
     # write data to silver
-    df.write.format("delta").mode("overwrite").save(f"gs://maseradb-silver/bigtable_last_month-{actual_year}-{actual_month}")
+    df.write.format("delta").mode("overwrite").save(f"gs://{bucketprefix}-silver/bigtable_last_month-{actual_year}-{actual_month}")
 
     # stop session
     spark.stop()   
